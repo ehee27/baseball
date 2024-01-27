f8660330bd1d5e181f2d5f7b62f82fe99da22625
Scott L <ehee27@gmail.com>
2024-01-27 13:07:10 -0600

    added messages data (model, controller)

---
+++ backend/models/Message.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose'
+
+const messageSchema = new mongoose.Schema(
+  {
+    creator: {
+      type: String,
+      required: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'Player',
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    thread: {
+      type: Array,
+      default: [],
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+// messageSchema.plugin(AutoIncrement, {
+//   inc_field: 'message',
+//   id: 'messageNums',
+//   start_seq: 100,
+// })
+
+const Message = mongoose.model('Message', messageSchema)
+
+export default Message
